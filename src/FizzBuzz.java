3dcaa52d8422058360499c85ffa4e9a2c23a2452
ZL-LIANG-fourm <z.liang@fourm.co.jp>
2024-06-17 14:21:10 +0900

    step_1_first_commit

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,19 +1,30 @@
 
 public class FizzBuzz {
 	
-	public FizzBuzz() {
-		
-		
-		
+	
+	public static void fizzbuzz(int max, int i) {
+		if (i > max) {
+			return;
+		}
+		if ((i % 3 ==0 )&& (i %5 ==0)) {
+			System.out.println("FizzBuzz");
+		} else if ((i % 3 ==0 )) {
+			System.out.println("Fizz");
+			
+		} else if ((i %5 ==0)) {
+			System.out.println("Buzz");
+		}else {
+			System.out.println(i);
+		}
+		i = i + 1;
+		fizzbuzz(max, i);
 	}
 	
 	
 	
-	
-	
-	public void main(String[] args) {
+	public static void main(String[] args) {
 		
-		
+		fizzbuzz(30,1);
 	}
 
 }
